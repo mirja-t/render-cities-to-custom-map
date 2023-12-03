b14ca38d284e8d60c4e1a4f5727eaac426469e32
mirja-t <email@mirja-tschakarov.de>
2023-12-03 17:11:30 +0100

    add space for text on svg map

--- src/SvgMap/SvgMap.tsx
+++ src/SvgMap/SvgMap.tsx
@@ -25,39 +25,46 @@ export function SvgMap({
 	const { width, height } = map.size;
 	const fontSize = (width / window.innerWidth) * (baseFontSize / zoom)
 	const [cities, setCities] = useState<RenderCities[]>(map.getCities(mapdata.cities, fontSize));
-
+	
 	useEffect(() => {
 		setCities(map.getCities(mapdata.cities, fontSize));
 	}, [zoom]);
 	
+	const offset = fontSize * 5;
+
 	return (
 		<div 
 			style={{
-				width: `${100 * zoom}%`,
-				position: 'relative',
-				backgroundColor: '#efefef',
+				width: `${(width + offset * 2) * zoom}px`,
+				height: `${height * zoom}px`,
+				paddingLeft: `${100 - (100 / (100 + (offset * 100 / width))) * 100}%`
 			}}>
-			<svg 
-				id="map" 
-				x="0px" 
-				y="0px" 
-				viewBox={`0 0 ${width} ${height}`}
-				xmlSpace="preserve"
-				style={{ position: 'relative', zIndex: 2 }}>
-				<Cities 
-					cities={cities}
-					zoom={zoom} 
-					fontSize={fontSize} 
-					fontFamily={fontFamily}
-					color={color}/>
-			</svg>
 			<div style={{
-				position: 'absolute',
-				top: 0,
-				left: 0,
-				zIndex: 1,
-				width: '100%',
-				height: '100%',
-			}}>{ mapComponent(zoom) }</div>
+				position: 'relative',
+			}}>
+				<svg 
+					id="map" 
+					x="0px" 
+					y="0px" 
+					viewBox={`0 0 ${width + offset} ${height}`}
+					xmlSpace="preserve"
+					style={{ position: 'relative', zIndex: 2 }}>
+					<Cities 
+						cities={cities}
+						zoom={zoom} 
+						fontSize={fontSize} 
+						fontFamily={fontFamily}
+						color={color}/>
+				</svg>
+				<div style={{
+					position: 'absolute',
+					top: 0,
+					left: 0,
+					zIndex: 1,
+					width: '100%',
+					height: '100%',
+					paddingRight: `${100 - (100 / (100 + (offset * 100 / width))) * 100}%`
+				}}>{ mapComponent(zoom) }</div>
+			</div>
 		</div>)
 }
